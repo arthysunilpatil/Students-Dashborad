275062d2b9a739aa02a0d1d936bf8d86cd5018d0
Arthy Patil <154821321+arthysunilpatil@users.noreply.github.com>
2024-03-03 13:06:48 +0530

    Add files via upload

---
+++ Student-Dashboard-main/Student-Dashboard-main/timeTable.js
@@ -0,0 +1,245 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09:30-11:10 AM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    },
+    {   
+        time: '11:20-12:00 AM',
+        roomNumber: '38-718',
+        subject: '404(ED)',
+        type: 'Lecture'
+    },
+    {   
+        time: '12:00-12:40 PM',
+        roomNumber: '38-718',
+        subject: '401(C++)',
+        type: 'Lecture'
+    },
+    {
+        time: '12:40-01:20 PM',
+        roomNumber: '27-304Y',
+        subject: '409(EVS)',
+        type: 'Lecture'
+    },
+    {
+        time: '01:20-02:00 PM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Lecture'
+    },
+    {
+        time: '02:30-04:30 PM',
+        roomNumber: '27-304Y',
+        subject: 'C++(Girls)',
+        type: 'Practical'
+    }
+]
+const Tuesday =[
+    {   
+        
+        time: '09:30-11:10 AM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    },
+    {   
+        time: '11:20-12:00 AM',
+        roomNumber: '38-718',
+        subject: '405(ERP)',
+        type: 'Lecture'
+    },
+    {   
+        time: '12:00-12:40 PM',
+        roomNumber: '38-718',
+        subject: '401(C++)',
+        type: 'Lecture'
+    },
+    {   
+        time: '12:40-01:20 PM',
+        roomNumber: '27-304Y',
+        subject: '409(EVS)',
+        type: 'Lecture'
+    },
+    {
+        time: '01:20-02:00 PM',
+        roomNumber: '27-304Y',
+        subject: '403(PHP)',
+        type: 'Lecture'
+    },
+    {
+        time: '02:30-04:30 PM',
+        roomNumber: '27-304Y',
+        subject: 'PHP(Girls)',
+        type: 'Practical'
+    }
+]
+
+const Wednesday =[
+    {   
+        
+        time: '09:30-11:10 AM',
+        roomNumber: '27-304Y',
+        subject: 'PHP(BOYS)',
+        type: 'Practical'
+    },
+    {   
+        time: '11:20-12:00 AM',
+        roomNumber: '38-718',
+        subject: '404(ED)',
+        type: 'Lecture'
+    },
+    {
+        time: '12:00-12:40 PM',
+        roomNumber: '38-718',
+        subject: '401(C++)',
+        type: 'Lecture'
+    },
+    {
+        time: '12:40-01:20 PM',
+        roomNumber: '27-304Y',
+        subject: '405(ERP)',
+        type: 'Lecture'
+    },
+    {
+        time: '01:20-02:00 PM',
+        roomNumber: '27-304Y',
+        subject: '403(PHP)',
+        type: 'Lecture'
+    },
+    {
+        time: '02:30-04:30 PM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    }
+
+]
+
+const Thursday =[
+    {   
+        time: '09:30-11:10 AM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    },
+    {   
+        time: '11:20-12:00 AM',
+        roomNumber: '38-718',
+        subject: '409(EVS)',
+        type: 'Lecture'
+    },
+    {   
+        time: '12:00-12:40 PM',
+        roomNumber: '38-718',
+        subject: '405(ERP)',
+        type: 'Lecture'
+    },
+    {
+        time: '12:40-01:20 PM',
+        roomNumber: '27-304Y',
+        subject: '404(ED)',
+        type: 'Lecture'
+    },
+    {
+        time: '01:20-02:00 PM',
+        roomNumber: '27-304Y',
+        subject: '403(PHP)',
+        type: 'Lecture'
+    },
+    {
+        time: '02:30-04:30 PM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    }
+]
+
+const Friday =[
+    {   
+        time: '09:30-11:10 AM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    },
+    {   
+        time: '11:20-12:00 AM',
+        roomNumber: '38-718',
+        subject: '401(C++)',
+        type: 'Lecture'
+    },
+    {   
+        time: '12:00-12:40 PM',
+        roomNumber: '38-718',
+        subject: '402(Software Eng.)',
+        type: 'Lecture'
+    },
+    {
+        time: '12:40-01:20 PM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Lecture'
+    },
+    {
+        time: '01:20-02:00 PM',
+        roomNumber: '27-304Y',
+        subject: '404(ED)',
+        type: 'Lecture'
+    },
+    {
+        time: '02:30-04:30 PM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    }
+
+]
+
+const Saturday =[
+    {   
+        time: '09:30-11:10 AM',
+        roomNumber: '34-604',
+        subject: 'C++(Boys)',
+        type: 'Practical'
+    },
+    {   
+        time: '11:20-12:00 AM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Lecture'
+    },
+    {   
+        time: '12:00-12:40 PM',
+        roomNumber: '38-718',
+        subject: '405(ERP)',
+        type: 'Lecture'
+    },
+    {
+        time: '12:40-01:20 PM',
+        roomNumber: '27-304Y',
+        subject: '409(EVS)',
+        type: 'Lecture'
+    },
+    {
+        time: '01:20-02:00 PM',
+        roomNumber: '27-304Y',
+        subject: '402(Software Eng.)',
+        type: 'Lecture' 
+    },
+    {
+        time: '02:30-04:30 PM',
+        roomNumber: '--',
+        subject: 'OFF',
+        type: 'Practical'
+    }
+    
+]
